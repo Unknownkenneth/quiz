41e1eecd31a46d7101ca639c30c33d063c0d1f33
Kenneth Louis <lowkam4@gmail.com>
2019-01-09 22:56:55 +0000

    Still stuck but added and fixed some JS

--- js/script.js
+++ js/script.js
@@ -1,41 +1,64 @@
 /* global $ */
-var userin=
-$(document).ready(function() {
-    $("button").click(function() {
-        var name = $("#name").val();
+var userin;
+var name = $("#name").val();
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        var totalScore;
-    });
-
-});
- function num1(){
-  if(q1Result === "Tell a teacher"){
-        totalScore=3;
-}  else if( q1Result=== "I'm tired of this"){
-            totalScore=7;
-}  else if ( q1Result==="fight back"){
-            totalScore=5;
-}
-  else if( q1Result=== "run away"){
-            totalScore=1
-}
-}
- return  totalScore = num1
+        var totalScore = 0
+        
+//$(document).ready(function() {
+   $("button").click(function() {
+        
  
-  function num2(){
-  if(q2Result === "Tell a teacher"){
-        totalScore=3;
-}  else if( q2Result=== "I'm tired of this"){
-            totalScore=7;
-}  else if ( q2Result==="fight back"){
-            totalScore=5;
+
+ function q1Result(){
+  if(q1Result === "Tell a teacher"){
+        totalScore= totalScore + 3;
+}  else if( q1Result === "I'm tired of this"){
+            totalScore= totalScore + 7;
+}  else if ( q1Result ==="fight back" || "Fight back" ){
+          totalScore= totalScore + 5;
+}
+  else if( q1Result === "run away" || "Run away"){
+            totalScore= totalScore + 1;
+}
+totalScore= totalScore +q1Result
+}
+ 
+ 
+  function q2Result(){
+  if(q2Result === "Yes"){
+          totalScore= totalScore +3;
+}  else if( q2Result=== "No "){
+          totalScore= totalScore + 7;
+}  else if ( q2Result==="Depends"){
+           totalScore= totalScore+ 5;
 }
   else if( q2Result=== "run away"){
-            totalScore=1
+           totalScore= totalScore +1;
 }
+ 
 }
 
+ 
+  function q3Result(){
+  if(q3Result === "Yes"){
+      totalScore= totalScore +3;
+}  else if( q3Result === "No "){
+              totalScore= totalScore +7;
+}  else if ( q3Result==="Depends"){
+              totalScore= totalScore +5;
+}
+  else if( q3Result=== "Yeah"){
+           totalScore= totalScore +1;
+
+}
+
+}
+
+
+});
+
+   //});
